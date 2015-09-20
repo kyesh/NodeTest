4d0e18c1f91c5cca5e19d9a0627404c3031db01d
unknown <kenyesh@gmail.com>
2015-09-20 01:36:05 -0400

    changed back to 8080

--- HelloWorld.js
+++ HelloWorld.js
@@ -7,8 +7,8 @@ var server = http.createServer(function (request, response) {
   response.end("Hello World\n");
 });
 
-// Listen on port 80, IP defaults to 127.0.0.1
-server.listen(80);
+// Listen on port 8080, IP defaults to 127.0.0.1
+server.listen(8080);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:80/");
+console.log("Server running at http://127.0.0.1:8080/");
